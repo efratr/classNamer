50d34dd4ccba0f354ab0f9df968800a2d890b8dc
efratr <37476556+efratr@users.noreply.github.com>
2018-08-08 22:14:14 +0300

    database

--- dal.php
+++ dal.php
@@ -1,8 +1,15 @@
 <?php
 
 function _connectToDB(){
-	mysql_connect("localhost","root","") or die(mysql_error());
-	mysql_select_db("main") or die("Cannot connect to database");
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
 }
 
 function insertWords($words){
