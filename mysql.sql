1bf0afa6dd59b6f44563cd4a742d7d22c351d7f2
efratr <37476556+efratr@users.noreply.github.com>
2018-08-10 11:12:04 +0300

    sql

--- mysql.sql
+++ mysql.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `words` (
- `word` varchar(50) CHARACTER SET ascii NOT NULL,
- `amount` int(11) NOT NULL DEFAULT '0',
- PRIMARY KEY (`word`)
-)
+CREATE TABLE ovWJU5rBsT.words (
+   word varchar(50) NOT NULL,
+   amount int(11) NOT NULL DEFAULT '0',
+   PRIMARY KEY (word)
+ );
